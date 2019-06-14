5d58f1f30271f7fdd539a83a5e243253c8fdc730
Valentin-K <51781067+Valentin-K@users.noreply.github.com>
2019-06-14 10:16:44 +0200

    Add files via upload

--- rVSV/Script1.R
+++ rVSV/Script1.R
@@ -2,14 +2,19 @@ library(DESeq2)
 library(tidyverse)
 counts <- read.delim("counts.tsv", stringsAsFactors=F)
 
-countdata <- counts
-keep <- rowSums(countdata) >5
+countdata <- counts %>%
+  column_to_rownames("X") %>% # turn the geneid column into rownames
+  as.matrix()
+head(countdata)
+
+
+keep <- rowSums(countdata, na.rm=F) >5
 countdata <- countdata[keep,]
 dim(countdata)
 
 #Bar plot of library sizes
 librarySizes <- colSums(countdata)
-barplot (libraarySizes,
+barplot (librarySizes,
          names = names(librarySizes),
          las=2,
          main="Barplot of library sizes")
