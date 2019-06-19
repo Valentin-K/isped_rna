0ca0e76cb892ee6c1614d93781fb03cec7d2d4c3
Valentin-K <51781067+Valentin-K@users.noreply.github.com>
2019-06-19 10:50:33 +0200

    Add files via upload

--- Script1.R
+++ Script1.R
@@ -1,5 +1,10 @@
 library(DESeq2)
 library(tidyverse)
+
+
+## Import Data
+## -----------
+
 counts <- read.delim("counts.tsv", stringsAsFactors=F)
 
 countdata <- counts %>%
@@ -7,10 +12,57 @@ countdata <- counts %>%
   as.matrix()
 head(countdata)
 
+# Get the total number of counts per gene
+cpg <- rowSums(countdata, na.rm=F)
 
-keep <- rowSums(countdata, na.rm=F) >500000
-countdata <- countdata[keep,]
-dim(countdata)
+# Test the nullity of expression of each gene
+nullGenes_test <- cpg==0
+
+# Get the indices of the non expressed genes
+nullIndices <- which(nullGenes_test)
+
+# Get the total number of non expressed genes
+nullNumber = length(nullIndices)
+
+# New matrix with only expressed genes
+onlyexpressed <- countdata[-nullIndices,]
+
+# Choose only well expressed genes
+# keep <- rowSums(, na.rm=F) >500000
+# countdata <- countdata[keep,]
+# dim(countdata)
+
+
+## Get meta-data of each sample
+## ----------------------------
+separated = strsplit(colnames(onlyexpressed), "_", fixed = TRUE, useBytes = F)
+do.call(rbind,separated)
+
+
+# v1 = lapply(separated, '[',1)
+# v2 = lapply(separated, '[',2)
+# for (i in v1) separated2 = strsplit(v1[i], " ")
+
+
+
+# DesEa2 DataObject creation from counts.tsv
+
+dds <- DESeqDataSetFromMatrix(countData = onlyexpressed, # matrix for DESEQ2 analysis
+                              colData = coldata, # Rows of colData correspond to columns of countData
+                              design = ~ condition) #  formula corresponding to each DDSObject, 
+# e.g., ~ group + condition, and designs with interactions, e.g., ~ genotype + treatment + genotype:treatment,
+#the last variable in the formula for building results tables and plotting
+
+dds
+
+
+
+
+
+
+
+
+
 
 #Bar plot of genes expression 
 expression <- rowSums(countdata)
