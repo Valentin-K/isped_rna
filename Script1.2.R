871708773e4acf83270fd031993da6ba04303dff
Valentin-K <51781067+Valentin-K@users.noreply.github.com>
2019-06-24 09:56:28 +0200

    Add files via upload

--- Script1.2.R
+++ Script1.2.R
@@ -2,6 +2,7 @@ library(DESeq2)
 library(tidyverse)
 library("pasilla")
 
+
 ## Import Data
 ## -----------
 
@@ -27,23 +28,81 @@ onlyexpressed <- countdata[-nullIndices,]
 ## ----------------------------
 
 
-coldata = read.delim("counts.tsv", row.names = 1)
+coldata = colnames(onlyexpressed)
 
-
-# separated = strsplit(colnames(onlyexpressed), "_", fixed = TRUE, useBytes = F)
-# coldata = do.call(rbind,separated)
+separated = strsplit((coldata), "_", fixed = TRUE, useBytes = F)
+coldata = cbind(coldata, do.call(rbind,separated))
 
 # v1 = lapply(separated, '[',1)
 # v2 = lapply(separated, '[',2)
 
 
-# DesEa2 DataObject creation from counts.tsv
 
-
+## Deseq2 data object created
+## ----------------------------
+colnames(coldata) [c(2,3)] <- c("id", "day")
 dds <- DESeqDataSetFromMatrix (countData = onlyexpressed, # matrix for DESEQ2 analysis
                               colData = coldata, # Rows of colData correspond to columns of countData
-                              design = ~ ) #  formula corresponding to each DDSObject, 
+                              design = ~ V2 + V3 ) #  V2 is subject number anf V2 is day from vaccination
+
+#formula corresponding to each DDSObject, 
 # e.g., ~ group + condition, and designs with interactions, e.g., ~ genotype + treatment + genotype:treatment,
 #the last variable in the formula for building results tables and plotting
 
-dds
+
+# explicit reference level 
+dds$V2 = relevel(dds$V3, ref = "d0" )
+
+# results d0 vs other days :
+dds <- DESeq(dds)
+res = results(dds)
+resultsNames(dds) # different days (d1,d3,d7) comparison vs d0
+
+# result for adjusted p, alpha =0.05 cause deafault is 0.1 alpha
+res05 <- results(dds, alpha=0.05)
+
+# remove noise = log fold change shrinkage (always for biological samples )
+resLFC = lfcShrink(dds,  coef = "V3_d1_vs_d0", type = "apeglm")
+# apeglm method for effect size shrinkage improving on the previous estimator
+
+# order results by smallest p-value 
+resOrdered = res[order(res$pvalue),]
+
+# How many adjusted p-values were less than 0.05?
+sum(res$padj < 0.05, na.rm=T)
+
+
+
+## Independent Hypothesis Weighting
+## ----------------------------
+
+#Data-driven hypothesis weighting increases detection power in genome-scale mult testing
+library ("IHW")
+resIHW = results (dds, filterFun = ihw)
+summary (resIHW)
+
+
+## Exploring and exporting results
+## ----------------------------
+
+# MA-plot for alpha 0.05 non shrunk results
+plotMA(res05, ylim=c(-2,2))
+# MA-plot for shrunken log2 LFC, that removes noise
+plotMA(resLFC, ylim=c(-2,2))
+# use the function identify to interactively detect the row number of individual genes 
+# by clicking on the plot
+# idx = identify(res$baseMean, res$log2FoldChange)
+# rownames(res) [idx]
+
+
+# Alternative shrinkage estimators
+
+
+
+
+
+
+
+
+
+
